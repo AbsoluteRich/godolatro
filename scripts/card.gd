c1668b6ee8fb7dedbef40b502827b0b8cee5e0d9
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-06-04 15:21:38 +1000

    Baby's first sorting algorithm

--- scripts/card.gd
+++ scripts/card.gd
@@ -3,19 +3,31 @@ extends TextureRect
 
 # https://www.reddit.com/r/godot/comments/13pm5o5/comment/ktmmqp0/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 static var scene: PackedScene = preload("res://scenes/card.tscn")
-var rank: int
-var suit: String
-@export var selected: bool
 static var all_selected: int = 0
 
+enum Suits { Clubs, Diamonds, Hearts, Spades }
 
-static func create(new_suit: String, new_rank: int) -> Card:
+var rank: int
+var suit: Suits
+
+@export var selected: bool
+
+
+static func create(new_suit: Suits, new_rank: int) -> Card:
 	var new_card: Card = scene.instantiate()
 	new_card.suit = new_suit
 	new_card.rank = new_rank
+	new_card.texture = load("res://assets/cards/card%s.png" % new_card)
+	return new_card
+
+
+func _to_string() -> String:
+	var properSuit: String
 	var properRank: String
 
-	match new_rank:
+	properSuit = Suits.find_key(self.suit)
+
+	match self.rank:
 		11:
 			properRank = "J"
 		12:
@@ -25,10 +37,9 @@ static func create(new_suit: String, new_rank: int) -> Card:
 		14:
 			properRank = "A"
 		_:
-			properRank = str(new_rank)
+			properRank = str(self.rank)
 
-	new_card.texture = load("res://assets/cards/card%s%s.png" % [new_card.suit, properRank])
-	return new_card
+	return properSuit + properRank
 
 
 # https://www.reddit.com/r/godot/comments/1gfl4m7/comment/luigfzf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
--- scripts/poker.gd
+++ scripts/poker.gd
@@ -1,14 +1,48 @@
 extends Control
 
-const suits: Array[String] = ["Clubs", "Diamonds", "Hearts", "Spades"]
 var deck: Array[Card] = []
 
 
 func construct_deck() -> void:
-	for suit in suits:
-		for rank in range(2, 13):
+	for suit in Card.Suits.values():
+		for rank in range(2, 15):
+			# Behind the scenes, Enums are technically just an array of ints
+			# Therefore, something that accepts an Enum as a parameter needs a int, not a String
 			deck.append(Card.create(suit, rank))
 	deck.shuffle()
+	assert(deck.size() == 52)
+
+
+func sort_cards(card_list: Array[Node]) -> Array[Node]:
+	var list_length: int = len(card_list)
+
+	for i in range(list_length):
+		var swapped: bool = false
+
+		for j in range(0, list_length - i - 1):
+			# Swap is set to true if the FIRST card is "more" than the SECOND card
+			var swap: bool = false
+
+			# Rank sorting
+			if card_list[j].rank > card_list[j + 1].rank:
+				swap = true
+
+			# Suit sorting (uses the rule Clubs > Diamonds > Hearts > Spades)
+			# Because there can't be duplicate suits (at least for now), this always produces a definitely sorted list
+			elif card_list[j].rank == card_list[j + 1].rank:
+				if card_list[j].suit > card_list[j + 1].suit:
+					swap = true
+
+			if swap:
+				var temp: Card = card_list[j]
+				card_list[j] = card_list[j + 1]
+				card_list[j + 1] = temp
+				swapped = true
+
+		if swapped == false:
+			break
+
+	return card_list
 
 
 func draw_card() -> Card:
@@ -37,7 +71,7 @@ func _ready() -> void:
 func _on_play_button_pressed() -> void:
 	for child in %Cards.get_children():
 		if child.selected:
-			print(child.rank, child.suit)
+			print(child)
 
 
 func _on_discard_button_pressed() -> void:
@@ -46,3 +80,14 @@ func _on_discard_button_pressed() -> void:
 			Card.all_selected -= 1
 			child.queue_free()
 			deal_cards(1)
+
+
+func _on_sort_button_pressed() -> void:
+	var all: Array[Node] = %Cards.get_children()
+	var sorted: Array[Node] = sort_cards(%Cards.get_children().duplicate())
+
+	for node in all:
+		print("ALL: ", node)
+
+	for node in sorted:
+		print("SORTED: ", node)
