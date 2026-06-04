f68e99d1a56945bab8139656eb55368ce3f12897
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-06-04 12:29:29 +1000

    Deck foundations

--- scripts/poker.gd
+++ scripts/poker.gd
@@ -1,31 +1,53 @@
 extends Control
 
-var rng = RandomNumberGenerator.new()
-const suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+const suits: Array[String] = ["Clubs", "Diamonds", "Hearts", "Spades"]
+var deck = []
+
+
+func construct_deck():
+	for suit in suits:
+		for rank in range(2, 13):
+			var properRank: String
+
+			match rank:
+				11:
+					properRank = "J"
+				12:
+					properRank = "Q"
+				13:
+					properRank = "K"
+				14:
+					properRank = "A"
+				_:
+					properRank = str(rank)
+
+			deck.append(suit + properRank)
+	deck.shuffle()
+
+
+func draw_card():
+	var chosenCard: String
+	if deck.is_empty():
+		return "Clubs2"  # Todo: Special logic required
+	
+	chosenCard = deck[0]
+	deck.pop_at(0)
+	return chosenCard
 
 
 func draw_hand():
+	for child in %Cards.get_children():
+		child.queue_free()
+	
 	for i in range(7):
-		var suit = suits.pick_random()
-		var rank = rng.randi_range(2, 14)
+		var cardData = draw_card()
 		var card = TextureRect.new()
-
-		match rank:
-			11:
-				rank = "J"
-			12:
-				rank = "Q"
-			13:
-				rank = "K"
-			14:
-				rank = "A"
-
-		card.texture = load("res://assets/cards/card%s%s.png" % [suit, rank])
-
+		card.texture = load("res://assets/cards/card%s.png" % cardData)
 		%Cards.add_child(card)
 
 
 func _ready():
+	construct_deck()
 	draw_hand()
 
 
